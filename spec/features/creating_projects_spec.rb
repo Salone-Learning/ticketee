31d88c882c5e8bdda2fd64fce607811ac3a04f9d
Salone Gupta <salonegupta@gmail.com>
2015-11-26 01:18:45 +0530

    Added validation for Project name.

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -6,10 +6,12 @@ end
 
 feature 'Creating projects' do
 
-  scenario 'can create a project' do
+  before do
     visit '/'
-
     click_link 'New Project'
+  end
+
+  scenario 'can create a project' do
     fill_in 'Name', with: 'TextMate2'
     fill_in 'Description', with: 'A text editor for OS X'
     click_button 'Create Project'
@@ -21,5 +23,12 @@ feature 'Creating projects' do
 
     title = "TextMate2 - Projects - Ticketee"
     expect(page).to have_title(title)
+  end
+
+  scenario 'can not create a project without a name' do
+    click_button 'Create Project'
+
+    expect(page).to have_content('Project has not been created.')
+    expect(page).to have_content('Name can\'t be blank')
   end
 end
